21df0b99d0fc8f20a0c74e1d0179e80cd6f691cd
habbababbai <szymanski.j.dev@gmail.com>
2026-05-24 13:01:30 +0200

    [MOBILE] - Eslint fixes

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
+const path = require('path');
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+
+const tsconfigRootDir = path.dirname(require.resolve('./tsconfig.json'));
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir,
+      },
+    },
   },
 ]);
